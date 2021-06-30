77403996614c48b70738a27ffacf6294f15d4c18
Arthur Metekohy <a.metekohy@hotmail.com>
2021-06-30 02:48:06 +0200

    added seperate folder for commands with the command clear to clear messages

---
+++ commands/clear.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'clear',
+  description: 'Clears messages!',
+  async execute(client, message, args) {
+      if(!args[0]) return message.reply("please enter the amount of messages that you want to clear!");
+      if(isNaN(args[0])) return message.reply("please enter a real number!");
+
+      if(args[0] > 100) return message.reply("you cannot delete more than 100 messages!");
+      if(args[0] < 1) return message.reply("you must delete at least one message!");
+
+      await message.channel.messages.fetch({limit: args[0]}).then(messages => {
+          message.channel.bulkDelete(messages);
+      });
+  }
+};
